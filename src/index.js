be589fb46381fef63477881d9238e38b103f6f2d
Marc Pont <slayerbleast@gmail.com>
2021-09-28 14:57:37 +0200

    Queries example

--- src/index.js
+++ src/index.js
@@ -3,12 +3,43 @@ const {ApolloServer} = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
+let links = [
+    {
+        id: 'link-1',
+        url: 'www.example1.com',
+        description: 'Example 1'
+    },
+    {
+        id: 'link-2',
+        url: 'www.example2.com',
+        description: 'Example 2'
+    },
+    {
+        id: 'link-3',
+        url: 'www.example3.com',
+        description: 'Example 3'
+    }
+]
+
 const resolvers = {
     Query: {
-        info: () => `Hello world!`
+        info: () => 'Hello world',
+        feed: () => links,
+    },
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
     }
 }
 
