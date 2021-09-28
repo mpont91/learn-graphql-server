0177534e4bf32cca5fd84891a5e91b7715598ebf
Marc Pont <slayerbleast@gmail.com>
2021-09-28 15:44:55 +0200

    Implement CRUD

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 const {ApolloServer} = require('apollo-server');
-
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`
+const fs = require('fs');
+const path = require('path');
 
 let links = [
     {
@@ -35,16 +24,44 @@ const resolvers = {
     Query: {
         info: () => 'Hello world',
         feed: () => links,
+        link: (parent, args) => links.find(item => item.id === args.id)
     },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
-    }
+    Mutation: {
+        post: (parent, args) => {
+            let idCount = links.length + 1;
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        },
+        updateLink: (parent, args) => {
+            const index = links.findIndex(item => item.id === args.id);
+            const link = links[index];
+            const linkUpdated = {
+                id: link.id,
+                description: args.description ? args.description : link.description,
+                url: args.url ? args.url : link.url,
+            }
+            links[index] = linkUpdated;
+            return linkUpdated;
+        },
+        deleteLink: (parent, args) => {
+            const index = links.findIndex(item => item.id === args.id);
+            const link = links[index];
+            links.splice(index, 1);
+            return link;
+        }
+    },
 }
 
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
     resolvers,
 })
 
